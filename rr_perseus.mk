1f88c81b9f6ea1c587b7c81402ddd858812140f2
Matt Evangelisti <mjevange@gmail.com>
2019-05-16 12:15:56 -0400

    fix perseus make file for RR

--- rr_perseus.mk
+++ rr_perseus.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/perseus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_perseus
+PRODUCT_NAME := rr_perseus
 PRODUCT_DEVICE := perseus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3
