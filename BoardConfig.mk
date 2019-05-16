a621364be380426e5cc0d238184568bfa53062c2
Matt Evangelisti <mjevange@gmail.com>
2019-05-16 12:13:10 -0400

    pring up perseus for RR

---
+++ BoardConfig.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm845-common
+-include device/xiaomi/sdm845-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/perseus
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := perseus
+
+# Kernel
+BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_CONFIG := perseus_defconfig
+
+# Partitions
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+
+# NFC
+TARGET_USES_NQ_NFC := true
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/perseus/BoardConfigVendor.mk
+
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+
+# VNDK
+BOARD_VNDK_RUNTIME_DISABLE := false
