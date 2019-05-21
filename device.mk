11c9714da7b8b4fb6e576ee4e3825a8a383ea01c
Demon000 <demonsingur@gmail.com>
2019-05-21 03:17:01 +0200

    perseus: add devicesettings for slider handling
    Change-Id: Ibd842d6ac6ea74c3921d53b28f18b669a8f7e07a

--- device.mk
+++ device.mk
@@ -64,3 +64,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+# Slider settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
