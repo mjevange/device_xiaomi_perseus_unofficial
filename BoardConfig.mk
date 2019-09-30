81f7975692dbf81917c0c28d88edc375ced9169d
Matt Evangelisti <mjevange@gmail.com>
2019-09-30 14:38:12 -0400

    add kernel source to device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ TARGET_USES_DRM_PP := true
 # Kernel
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := perseus_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
 # Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
